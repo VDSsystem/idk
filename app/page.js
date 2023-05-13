8a8ff657d9a077ea4990e4cedc3d34141f8f02bd
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 08:06:15 +0300

    www

--- app/page.js
+++ app/page.js
@@ -126,12 +126,13 @@ function RootPage() {
     data.append('file', file);
     data.append('upload_preset', 'myUploads');
     data.append("api_key", '231941467471291');
+    let imageUrl;
   try {
     const response = await fetch('https://api.cloudinary.com/v1_1/pdfuuif0cy/image/upload', {
       method: 'POST',
       body: data
     }).then(r => r.json());
-    const imageUrl = response.url;
+    imageUrl = response.url;
     console.log(imageUrl); // log the URL to the console
   } catch (error) {
     console.error(error);
