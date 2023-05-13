2dee213148edac52f6dc27fdb077495651835a69
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-13 23:01:55 +0300

    final?

--- app/page.js
+++ app/page.js
@@ -79,33 +79,40 @@ function RootPage() {
       const height = y2 - y1;
 
       if (classesData[i] == 0) {
-        ctx.fillStyle = "#FF0000"; // red
+        //ctx.fillStyle = "#FF0000"; // red
         // draw the bounding box
         ctx.strokeStyle = "#FF0000";
         ctx.lineWidth = 2;
         ctx.strokeRect(x1, y1, width, height);
+        const label = klass + " - " + score + "%";
+        const textWidth = ctx.measureText(label).width;
+        const textHeight = parseInt(font, 10); // base 10 
+        // draw the label background
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(x1 - 1, y1 - (textHeight + 4), textWidth + 6, textHeight + 4);
+
+        // draw the label text
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(label, x1 + 2, y1 - (textHeight + 2));
 
       } else if (classesData[i] == 1){
-        ctx.fillStyle = "#0000FF"; // blue
+        //ctx.fillStyle = "#0000FF"; // blue
          // draw the bounding box
         ctx.strokeStyle = "#0000FF";
         ctx.lineWidth = 2;
         ctx.strokeRect(x1, y1, width, height);
+        const label = klass + " - " + score + "%";
+        const textWidth = ctx.measureText(label).width;
+        const textHeight = parseInt(font, 10); // base 10 
+        // draw the label background
+        ctx.fillStyle = "#0000FF";
+        ctx.fillRect(x1 - 1, y1 - (textHeight + 4), textWidth + 6, textHeight + 4);
+
+        // draw the label text
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(label, x1 + 2, y1 - (textHeight + 2));
       }
 
-      const label = klass + " - " + score + "%";
-      const textWidth = ctx.measureText(label).width;
-      const textHeight = parseInt(font, 10); // base 10
-      console.log(klass);
-      
-
-      // draw the label background
-      ctx.fillStyle = "#C53030";
-      ctx.fillRect(x1 - 1, y1 - (textHeight + 4), textWidth + 6, textHeight + 4);
-
-      // draw the label text
-      ctx.fillStyle = "#FFFFFF";
-      ctx.fillText(label, x1 + 2, y1 - (textHeight + 2));
     }
   };
 
