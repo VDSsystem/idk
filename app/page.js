0f4de5b85bebb37c22b27463c58579ea810f21ca
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-14 18:32:06 +0300

    ,,,,,,,,,,

--- app/page.js
+++ app/page.js
@@ -182,20 +182,21 @@ function RootPage() {
   } catch (error) {
     console.error(error);
   }
-  try {
+  try{
     const url = imageURLRef.current;
     const params = new URLSearchParams();
     params.append('url', url);
     const response2 = await fetch("https://vadss.vercel.app/api/output", {
       method: 'POST',
       body: params,
-    });
-    const data2 = await response2.json();
-    const id = data2.id;
+    }).then(r => r.json());
+    const id = response2.id;
     console.log(id);
-  } catch(err) {
+  }
+  catch(err){
     console.log(err);
   }
+  
   
 }
 
